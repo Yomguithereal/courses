a3b7731351bcf2d9f8d92895ccb73417e7955522
Yomguithereal <guillaumeplique@gmail.com>
2017-04-18 22:20:15 +0200

    Adding regression model

--- perfectionnement/2017/projects/spam/model.py
+++ perfectionnement/2017/projects/spam/model.py
@@ -5,26 +5,26 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import precision_score, recall_score
 
 # Loading the files
-print 'Loading SVM file...'
+print('Loading SVM file...')
 X, y = load_svmlight_file('webspam-unigrams.svm')
 
 # Splitting train & test
-print 'Splitting dataset...'
+print('Splitting dataset...')
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.1)
 
 # Training the model
-print 'Training the model...'
+print('Training the model...')
 clf = MultinomialNB()
 clf.fit(X_train, y_train)
 
 # Testing
-print 'Testing...'
+print('Testing...')
 y_predicted = [clf.predict(x) for x in X_test]
 
 # Scores
-print 'Computing scores...'
+print('Computing scores...')
 precision = precision_score(y_test, y_predicted)
 recall = recall_score(y_test, y_predicted)
 
-print 'Precision: ', precision
-print 'Recall: ', recall
+print('Precision: ', precision)
+print('Recall: ', recall)
